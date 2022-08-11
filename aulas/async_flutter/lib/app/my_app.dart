8c212680da4c9cd0a4b6d83d98e1f46a951abee1
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-08-11 16:41:36 -0300

    criando um novo projeto dart.

---
+++ aulas/async_flutter/lib/app/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:async_flutter/app/routes/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
